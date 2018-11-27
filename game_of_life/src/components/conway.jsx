617eb747a47b231e3d47c7d7612f1d9405a43d32
javilaotz-at-346627697124 <javilaortiz@gmail.com>
2018-11-27 14:08:03 -0500

    Changing code to ES6

--- game_of_life/src/components/conway.jsx
+++ game_of_life/src/components/conway.jsx
@@ -8,60 +8,72 @@ export default class conway extends Component {
             height:50,
             width:50,
             theGrid: [...Array(50)].map(x => Array(50).fill(0)),
-            mirrorGrid: [...Array(50)].map(x => Array(50).fill(0)),
-            c: null,
-            ctx: null
+            mirrorGrid: [...Array(50)].map(x => Array(50).fill(0))
         }   
-
+        this.updateGrid = this.updateGrid.bind(this)
     }
-    updateTheGrid(x,y,value){
-        this.setState({
-            theGrid: update(this.state.theGrid, 
-                { [x]: { [y]: { $set: value } } }
-            ) 
+
+    //Set a cell into TheGrid according to x and y position
+    updateTheGrid(x,y,cell){
+        let newArray = update(this.state.theGrid, {
+            [x]: {
+                [y]: { $set: cell }
+            }
         })
-        console.log(this.state.theGrid, "theGrid")
+        this.setState(this.state.theGrid = newArray)
     }
 
     //Setting live cells
     populateGrid(){
-        /*
-        var theGrid = this.state.theGrid
-        theGrid[0][0].value = 1;
-        theGrid[0][1] = 1;
-        theGrid[1][0] = 1;
-        theGrid[1][3] = 1;
-        theGrid[2][1] = 1;
-        theGrid[2][2] = 1;
-        */
         this.updateTheGrid(0,0,1)
-        
-        
+        this.updateTheGrid(0,1,1)
+        this.updateTheGrid(1,0,1)
+        this.updateTheGrid(1,3,1)
+        this.updateTheGrid(2,1,1)
+        this.updateTheGrid(2,2,1)
     }
 
     //Plot Grid
     plotGrid() {
-        var theGrid = this.state.conways_grid;
+        var theGrid = this.state.theGrid;
+        const ctx = this.refs.canvas.getContext('2d');
         var liveCount = 0;
+
+        
+        /*
+        //This fragment is not working
+        theGrid.forEach(function (val, index) {
+            theGrid[index].forEach(function (val, index2){
+                if (theGrid[index][index2] === 1) {
+                    console.log(theGrid[index][index2], "plotting live cell at ("+index+","+index2+")")
+                    this.plotCell(index, index2);
+                    liveCount++;
+                }
+            })
+        })  */
+
         for (var j = 0; j < this.state.height; j++) {
             for (var k = 0; k < this.state.width; k++) {
                 if (theGrid[j][k] === 1) {
-                    console.log("plotting live cell at (" + j + ", " + k +")")
-                    this.plotCell(j, k);
+                    //console.log("plotting live cell at (" + j + ", " + k +")")
+                    this.plotCell(ctx, k, j);
                     liveCount++;
                 }
             }
         }
+        ctx.stroke();
+
+
     }
 
     //update the grid according to conway's rules
     updateGrid() { //perform one iteration of grid update
-        var theGrid = this.state.conways_grid;
-        var mirrorGrid = theGrid
-
-        for (var j = 1; j < this.state.height -1; j++) { //iterate through rows
-            for (var k = 1; k < this.state.width -1;  k++) { //iterate through columns
-                console.log("Iterating at ("+j+", "+k+")")
+        var theGrid = this.state.theGrid
+        var mirrorGrid = this.state.mirrorGrid
+        
+        console.log("mirrorGrid : ", mirrorGrid)
+        for (var j = 1; j < this.state.height -1; j++) {
+            for (var k = 1; k < this.state.width -1;  k++) {
                 var totalCells = 0;
                 //add up the total values for the surrounding cells
                 totalCells += theGrid[j - 1][k - 1]; //top left
@@ -89,53 +101,42 @@ export default class conway extends Component {
                         mirrorGrid[j][k] = 0; //
                 }
             }
+            for (var l = 0; l < this.state.height; l++) { //iterate through rows
+                //top and bottom
+                mirrorGrid[l][0] = mirrorGrid[l][this.state.height - 3];
+                mirrorGrid[l][this.state.height - 2] = mirrorGrid[l][1];
+                //left and right
+                mirrorGrid[0][l] = mirrorGrid[this.state.height - 3][l];
+                mirrorGrid[this.state.height - 2][l] = mirrorGrid[1][l];
+            }
         }
-
-        for (var l = 0; l < this.state.height; l++) { //iterate through rows
-            //top and bottom
-            mirrorGrid[l][0] = mirrorGrid[l][this.state.height - 3];
-            mirrorGrid[l][this.state.height - 2] = mirrorGrid[l][1];
-            //left and right
-            mirrorGrid[0][l] = mirrorGrid[this.state.height - 3][l];
-            mirrorGrid[this.state.height - 2][l] = mirrorGrid[1][l];
-
-        }
-
 
         //swap grids
         var temp = theGrid;
         theGrid = mirrorGrid;
         mirrorGrid = temp;
+
+        this.setState(this.state.theGrid = theGrid);
+        this.setState(this.state.mirrorGrid = mirrorGrid);
     }
 
     tick() { //main loop
-        this.updateGrid();
-        this.plotGrid();
+        this.updateGrid()
+        this.plotGrid()
     }
 
-
     //plotting a live cell
-    plotCell(x,y) {
-        const ctx = this.refs.canvas.getContext('2d');
+    plotCell(ctx,x,y) {
         ctx.fillRect(x*10,y*10,10,10);
     }
 
     componentDidMount() {
-        /*var theGrid = createArray(gridWidth);
-        var mirrorGrid = createArray(gridWidth);
-        var c = document.getElementById("myCanvas");
-        var ctx = c.getContext("2d");
-        ctx.fillStyle = "#FF0000";*/
-
-        
-        /*
-        
-        this.plotGrid();  */     
         this.populateGrid()
-
-        //setTimeout(this.tick(), 1000);
-        
+        this.plotGrid()        
+        this.updateGrid()
     }    
+
+
 
     render() {
         
