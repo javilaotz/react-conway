f1db40e3ee29c945dd12eadfbe5a45f574fc6e3f
javilaotz <javilaortiz@gmail.com>
2018-11-28 19:57:11 -0500

    Adding button to step and play, creating a function to get the totalCells amount easily

--- game_of_life/src/components/conway.jsx
+++ game_of_life/src/components/conway.jsx
@@ -11,6 +11,7 @@ export default class conway extends Component {
         this.updateCellTheGrid = this.updateCellTheGrid.bind(this)
         this.plotCell = this.plotCell.bind(this)
         this.tick = this.tick.bind(this)
+        this.action = this.action.bind(this)
     }
     
     //Set a cell into TheGrid according to x and y position
@@ -20,6 +21,7 @@ export default class conway extends Component {
   
         this.setState({theGrid:theGrid})
     }
+    //if this works fine the exersice will be ok (control input)
     glider(){
         this.updateCellTheGrid(0,1,1)
         this.updateCellTheGrid(1,2,1)
@@ -27,6 +29,7 @@ export default class conway extends Component {
         this.updateCellTheGrid(2,1,1)
         this.updateCellTheGrid(2,2,1)
     }
+    //excersise seed
     seed(){
         this.updateCellTheGrid(0,0,1)
         this.updateCellTheGrid(0,1,1)
@@ -38,7 +41,7 @@ export default class conway extends Component {
  
     //Setting live cells
     populateGrid(){
-        this.seed()
+        this.glider()
     }
  
     //Plot Grid
@@ -53,20 +56,34 @@ export default class conway extends Component {
                 }else{
                     this.killCell(ctx, col_index, row_index)
                 }
+                return 0;
             })
+            return 0;
         })
         
         ctx.stroke();
  
     }
+
+    checkCell(x,y,grid,totalCells){
+        if (x >= 0 && x <=49) {
+            if (y >= 0 && y <=49) {
+                totalCells += grid[x][y]
+            }
+        }
+        return totalCells
+    }
  
     //update the grid according to conway's rules
     updateGrid() { //perform one iteration of grid update
         let theGrid = this.state.theGrid || []
+        let mirrorGrid = theGrid
         
         theGrid.map((row, row_index) => {
             row.map((item, col_index) => {
-                let cellStatus = item        
+                    if (row_index === 2) {
+                        //debugger
+                    }    
                 
                     let totalCells = 0;  //amount of live neghtbour
                     
@@ -75,90 +92,62 @@ export default class conway extends Component {
                     let pos_y = row_index
 
                     //top left:
-                    if (pos_x - 1 >= 0) {
-                        if (pos_y - 1 >= 0) {
-                            totalCells += theGrid[pos_x-1][pos_y-1]
-                        }
-                    }
+                    totalCells = this.checkCell(pos_x - 1, pos_y - 1, theGrid, totalCells)
                     //top center:
-                    if (pos_x-1 >= 0) {
-                        if (pos_y >= 0) {
-                            totalCells += theGrid[pos_x - 1][pos_y]
-                        }
-                    }
+                    totalCells = this.checkCell(pos_x, pos_y - 1, theGrid, totalCells)
                     //top right:
-                    if (pos_x-1 >= 0) {
-                        if (pos_y+1 >= 0 && pos_y + 1 <=49) {
-                            totalCells += theGrid[pos_x - 1][pos_y + 1]
-                        }
-                    }
+                    totalCells = this.checkCell(pos_x + 1, pos_y - 1, theGrid, totalCells)
+
                     //middle left:
-                    if (pos_x >= 0) {
-                        if (pos_y - 1 >= 0) {
-                            totalCells += theGrid[pos_x][pos_y - 1]
-                        }
-                    }
+                    totalCells = this.checkCell(pos_x - 1, pos_y, theGrid, totalCells)
+                    //middle center:
+                    //not necessary, current cell position
                     //middle right:
-                    if (pos_x >= 0) {
-                        if (pos_y + 1 >= 0 && pos_y + 1 <=49) {
-                            totalCells += theGrid[pos_x][pos_y + 1]
-                        }
-                    }
-                    //bottom left:
-                    if (pos_x + 1 >= 0 && pos_x + 1 <=49) {
-                        if (pos_y - 1 >= 0) {
-                            totalCells += theGrid[pos_x + 1][pos_y - 1]
-                        }
-                    }
-                    //bottom center:
-                    if (pos_x + 1 >= 0 && pos_x + 1 <=49) {
-                        if (pos_y >= 0) {
-                            totalCells += theGrid[pos_x + 1][pos_y]
-                        }
-                    }
-                    //bottom right:
-                    if (pos_x+1 >= 0 && pos_x + 1 <=49) {
-                        if (pos_y+1 >= 0 && pos_y + 1 <=49) {
-                            totalCells += theGrid[pos_x + 1][pos_y + 1]
-                        }
-                    }
+                    totalCells = this.checkCell(pos_x + 1, pos_y, theGrid, totalCells)
+
+                    //top left:
+                    totalCells = this.checkCell(pos_x - 1, pos_y + 1, theGrid, totalCells)
+                    //top center:
+                    totalCells = this.checkCell(pos_x, pos_y + 1, theGrid, totalCells)
+                    //top right:
+                    totalCells = this.checkCell(pos_x + 1, pos_y + 1, theGrid, totalCells)
+                   
                     /*
                     Any live cell with fewer than two live neighbors dies, as if by underpopulation.
                     Any live cell with two or three live neighbors lives on to the next generation.
                     Any live cell with more than three live neighbors dies, as if by overpopulation.
                     Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
                     */
-
                     switch (totalCells) {
                         case totalCells < 2 :
-                            if (cellStatus===1) {
-                                theGrid[pos_x][pos_y] = 0 //Dies by underpopulation  
+                            if (item===1) {
+                                mirrorGrid[pos_x][pos_y] = 0 //Dies by underpopulation  
                             }
                             break;
                         case totalCells===2:  
-                            if (cellStatus===1) {
-                                theGrid[pos_x][pos_y] = 1 //Lives
+                            if (item===1) {
+                                mirrorGrid[pos_x][pos_y] = 1 //Lives
                             }                            
                             break;
                         case totalCells===3:  
-                                theGrid[pos_x][pos_y] = 1 //Lives by reproduction or pass to the next generation              
+                                mirrorGrid[pos_x][pos_y] = 1 //Lives by reproduction or pass to the next generation              
                             break;
                         case totalCells > 3 :   
-                            if (cellStatus===1) {
-                                theGrid[pos_x][pos_y] = 0 //Dies by overpopulation
+                            if (item===1) {
+                                mirrorGrid[pos_x][pos_y] = 0 //Dies by overpopulation
                             }                         
                             break;
                         default:
-                            if (cellStatus===0) {
-                                theGrid[pos_x][pos_y] = 0                                
+                            if (item===0) {
+                                mirrorGrid[pos_x][pos_y] = 0                                
                             }
                     }
+                    return 0;
                 
             })
+            return 0;
         })
-
-        //console.log(`Grid resultante: ${theGrid}`)
-        this.setState({theGrid : theGrid})
+        this.setState({theGrid : mirrorGrid})
     }
  
     tick() { //main loop
@@ -176,23 +165,28 @@ export default class conway extends Component {
     killCell(ctx,x,y) {
         ctx.fillStyle="#FFF";
         ctx.fillRect(x*10,y*10,10,10);
-    }    
+    }   
+    
+    //Play button action
+    action(){
+        this.interval = setInterval(this.tick, 1000);
+    }
  
     componentDidMount() {
         this.populateGrid()
-        this.plotGrid() 
-        this.interval = setInterval(this.tick, 1000);        
+        this.plotGrid()     
     }    
     componentWillMount(){
-        //clearInterval(this.interval)
+        clearInterval(this.interval)
     }
  
     render() {
-        
         return (
         <div>
             <h1>Conway's game of life</h1>
            <canvas ref="canvas" width={500} height={500}  />
+           <button onClick={this.tick}>Step</button>
+           <button onClick={this.action}>Play (1 sec)</button>
        </div>
        )
    }
