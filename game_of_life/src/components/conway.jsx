ebf5c57fbec42c4ec003dcfedb9109170cf2f192
javilaotz <javilaortiz@gmail.com>
2018-11-29 20:42:10 -0500

    Dropping the lidegrid, changing menu and add livecell and generation counter, setting the timing into 1000ms

--- game_of_life/src/components/conway.jsx
+++ game_of_life/src/components/conway.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
-import { Row, Col, Container, Button } from 'reactstrap'
+import { Row, Col, Container, Button, Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    ButtonGroup  } from 'reactstrap'
  
 export default class conway extends Component {
     constructor(props){
         super(props);
         this.state = {
             theGrid: [...Array(50)].map(x => Array(50).fill(0)),
-            mirrorGrid: [...Array(50)].map(x => Array(50).fill(0))
+            mirrorGrid: [...Array(50)].map(x => Array(50).fill(0)),
+            generation: 0,
+            liveCells: 0
         }  
         this.updateGrid = this.updateGrid.bind(this)
         this.updateCellTheGrid = this.updateCellTheGrid.bind(this)
@@ -30,6 +38,24 @@ export default class conway extends Component {
         this.updateCellTheGrid(2,1,1)
         this.updateCellTheGrid(2,2,1)
     }
+    //having fun :)
+    smallExploder(){
+        //glider
+        this.updateCellTheGrid(0,1,1)
+        this.updateCellTheGrid(1,2,1)
+        this.updateCellTheGrid(2,0,1)
+        this.updateCellTheGrid(2,1,1)
+        this.updateCellTheGrid(2,2,1)
+
+        this.updateCellTheGrid(30,31,1)
+        this.updateCellTheGrid(31,30,1)
+        this.updateCellTheGrid(31,31,1)
+        this.updateCellTheGrid(31,32,1)
+        this.updateCellTheGrid(32,30,1)
+        this.updateCellTheGrid(32,32,1)
+        this.updateCellTheGrid(33,31,1)
+        
+    }
     //excersise seed
     seed(){
         this.updateCellTheGrid(0,0,1)
@@ -42,7 +68,7 @@ export default class conway extends Component {
  
     //Setting live cells
     populateGrid(){
-        this.glider()
+        this.smallExploder()
     }
  
     //Plot Grid
@@ -55,7 +81,7 @@ export default class conway extends Component {
                 if(col===1){
                     this.plotCell(ctx, col_index, row_index)
                 }else{
-                    this.killCell(ctx, col_index, row_index)
+                    this.deadCell(ctx, col_index, row_index)
                 }
                 return 0;
             })
@@ -79,6 +105,8 @@ export default class conway extends Component {
     updateGrid() { //perform one iteration of grid update
         let theGrid = this.state.theGrid || []
         let mirrorGrid = [...Array(50)].map(x => Array(50).fill(0))
+        let generation = this.state.generation
+        let liveCells = 0
         
         theGrid.map((row, row_index) => {
             row.map((item, col_index) => {
@@ -115,11 +143,13 @@ export default class conway extends Component {
                     if(totalCells === 2){
                         if (item===1) { //Is alive?
                             mirrorGrid[pos_x][pos_y] = 1 //Lives
+                            liveCells++
                         }  
                     } 
                     
                     if(totalCells === 3){
-                        mirrorGrid[pos_x][pos_y] = 1 //Lives by reproduction or pass to the next generation                
+                        mirrorGrid[pos_x][pos_y] = 1 //Lives by reproduction or pass to the next generation   
+                        liveCells++             
                     } 
                     
                     if(totalCells > 3){
@@ -129,6 +159,9 @@ export default class conway extends Component {
             })
             //return console.log("ending 2");
         })
+        generation++
+        this.setState({generation : generation})
+        this.setState({liveCells : liveCells})
         this.setState({theGrid : mirrorGrid})
     }
  
@@ -144,7 +177,7 @@ export default class conway extends Component {
     }
 
     //plotting a dead cell
-    killCell(ctx,x,y) {
+    deadCell(ctx,x,y) {
         ctx.fillStyle="#EEE";
         ctx.fillRect(x*10,y*10,10,10);
     }   
@@ -153,37 +186,10 @@ export default class conway extends Component {
     action(){
         this.interval = setInterval(this.tick, 200);
     }
-
-    //draw a background grid into the canvas
-    backgroundGrid(){
-        const ctx = this.refs.canvas.getContext('2d');     
-        let x = 10
-        let y = 10
-        let w = 500
-        let h = 500
-        ctx.canvas.width  = w;
-        ctx.canvas.height = h;
-        
-        
-        for (x=0;x<=w;x+=10) {
-            for (y=0;y<=h;y+=10) {
-                ctx.moveTo(x, 0);
-                ctx.lineTo(x, h);
-                ctx.stroke();
-                ctx.moveTo(0, y);
-                ctx.lineTo(w, y);
-            }
-        }
-        ctx.strokeStyle="#EEEEEE";
-        ctx.lineWidth=2;
-        ctx.stroke();
-    }
  
     componentDidMount() {
-        this.backgroundGrid()
         this.populateGrid()
         this.plotGrid()     
-        //this.interval = setInterval(this.tick, 1000);
     }    
     componentWillMount(){
         clearInterval(this.interval)
@@ -192,16 +198,30 @@ export default class conway extends Component {
     render() {
         return (
         <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">Conway's game of life</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <ButtonGroup>
+                            <Button size="sm" onClick={this.tick} color="secondary">Step</Button>
+                            <Button size="sm" onClick={this.action} color="secondary">Play</Button>
+                            <Button size="sm" color="secondary" href="https://github.com/javilaotz/react-conway">GitHub</Button>
+                        </ButtonGroup>
+                    </Nav>
+                </Collapse>
+            </Navbar>
             <Container>
                 <Row>
-                    <Col><h1>Conway's game of life</h1></Col>
+                    <Col><canvas ref="canvas" width={500} height={500}/></Col>
                 </Row>
                 <Row>
-                    <Col><canvas ref="canvas" width={500} height={500}  /></Col>
-                </Row>
-                <Row>
-                    <Col sm={{ size: 'small', offset: 1 }}><Button color="primary" onClick={this.tick}>Step</Button></Col>
-                    <Col sm={{ size: 'small', offset: 1 }}><Button color="success" onClick={this.action}>Play</Button></Col>
+                    <Col>
+                        Generation: {this.state.generation}
+                    </Col>
+                    <Col>
+                        Live Cells: {this.state.liveCells}
+                    </Col>
                 </Row>
             </Container>
        </div>
